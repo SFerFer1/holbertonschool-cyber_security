9a3163b72983ebafd40a779359dc47db666074be
SFerFer1 <10048@holbertonstudents.com>
2025-08-13 18:38:05 -0300

    ej 0,1

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | grep -iE "^Registrant|^Admin|^Tech"
+whois $1 | awk -F: '/^(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email)/ {gsub(/^ +| +$/, "", $2); print $1 "," $2}' > "$1.csv"
