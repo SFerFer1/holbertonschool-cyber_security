2c40384ad5650a7e0b72bd25141e35437b1770cb
SFerFer1 <10048@holbertonstudents.com>
2025-09-16 12:22:40 -0300

    ej 1 with readme

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 hash=${1#"{xor}"}
 decoded=$(echo "$hash" | base64 --decode)
-original=$(echo -n "$decoded" | perl -pe 's/(.)/chr(ord($1)^0xFF)/ge')
+original=$(echo -n "$decoded" | perl -pe 's/(.)/chr(ord($1)^0xAA)/ge')
 echo "$original"
