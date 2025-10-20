f3dedea8f27e0c541d65a70167bf9d4d5fd39f21
SFerFer1 <10048@holbertonstudents.com>
2025-10-20 10:55:56 -0300

    ej 3

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-SALT=$(openssl rand -hex 16); echo -n "${SALT}${1}" | sha512sum | cut -d ' ' -f 1 > 3_hash.txt
+SALT=$(openssl rand -hex 16); echo -n "${SALT}" "$1" | sha512sum | cut -d ' ' -f 1 > 3_hash.txt
