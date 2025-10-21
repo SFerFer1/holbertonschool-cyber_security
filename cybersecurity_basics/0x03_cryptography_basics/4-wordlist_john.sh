9bd8dbc1a01e2651d0c347a39eefe27e6de5bdc1
SFerFer1 <10048@holbertonstudents.com>
2025-10-21 09:06:09 -0300

    try 3 ej 4

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=dummy "$1" > 4-password.txt ^Password123
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=Raw-MD5 "$1"
