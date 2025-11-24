3a94b2fbf1ca75ebf93cf4bc7136bcc21587134c
SFerFer1 <10048@holbertonstudents.com>
2025-11-24 12:40:59 -0300

    ej 2

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 grep "$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')" logs.txt \
 | awk -F'"' '{print $4}' \
+| grep -v "^-$" \
 | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
