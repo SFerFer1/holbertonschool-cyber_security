f4528636cfc14ee1e714ccfe876784a8e3d38b33
SFerFer1 <10048@holbertonstudents.com>
2025-11-29 19:34:31 -0300

    a

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,62 +1,55 @@
 #!/usr/bin/python3
+
 """
-Heap edit
+Something
 """
 
 import sys
 
+
 def main():
     """
-    Main func
+    Something
     """
-
-    # Args check
-    if len(sys.argv) != 4:
-        print("Usage: pid text1 text2")
-        sys.exit(1)
-
     pid = sys.argv[1]
     search = sys.argv[2].encode("ascii")
     replace = sys.argv[3].encode("ascii")
 
     heap_start = None
     heap_end = None
-
-    # Read maps
     try:
         with open(f"/proc/{pid}/maps", "r") as maps:
             for line in maps:
                 if "[heap]" in line:
                     addr = line.split()[0]
-                    a, b = addr.split("-")
-                    heap_start = int(a, 16)
-                    heap_end = int(b, 16)
+                    tmp = addr.split("-")
+                    heap_start = int(tmp[0], 16)
+                    heap_end = int(tmp[1], 16)
                     break
-    except (FileNotFoundError, PermissionError):
-        print("Usage: pid text1 text2")
+    except FileNotFoundError:
+        print("Usage: read_write_heap.py pid search_string replace_string")
+        sys.exit(1)
+    except PermissionError:
+        print("Usage: read_write_heap.py pid search_string replace_string")
         sys.exit(1)
 
-    # No heap
     if heap_start is None:
-        print("Usage: pid text1 text2")
+        print("Usage: read_write_heap.py pid search_string replace_string")
         sys.exit(1)
 
-    # Read/write mem
     try:
         with open(f"/proc/{pid}/mem", "rb+") as mem:
             mem.seek(heap_start)
-            data = mem.read(heap_end - heap_start)
-            idx = data.find(search)
+            heap_data = mem.read(heap_end - heap_start)
+            index = heap_data.find(search)
 
-            if idx == -1:
-                print("Text not found")
+            if index == -1:
                 sys.exit(1)
 
-            mem.seek(heap_start + idx)
+            mem.seek(heap_start + index)
             mem.write(replace)
-            print("OK")
     except (PermissionError, OSError, IOError):
-        print("Usage: pid text1 text2")
+        print("Usage: read_write_heap.py pid search_string replace_string")
         sys.exit(1)
 
 
