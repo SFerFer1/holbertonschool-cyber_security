e2e09ef48b5d20063d4928b0e2047f4d80363ffc
SFerFer1 <10048@holbertonstudents.com>
2026-01-13 09:43:02 -0300

    ruby ej 4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -7,8 +7,7 @@ def merge_json_files(file1_path, file2_path)
     data1 = JSON.parse(content1)
     data2 = JSON.parse(content2)
 
-    merged_data = data1 + data2
+    merged_data = data2 + data1
 
-    merged_data
-
+    File.write(file2_path, JSON.pretty_generate(merged_data))
 end
