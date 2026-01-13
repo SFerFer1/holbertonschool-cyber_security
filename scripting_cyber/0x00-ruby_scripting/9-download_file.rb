5c27de29b80298d4afaf918908c18a5f403e6297
SFerFer1 <10048@holbertonstudents.com>
2026-01-13 11:15:31 -0300

    ruby ej 9

---
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+require 'uri'
+require 'fileutils'
+
+def download_file
+  if ARGV.length != 2
+    puts "Usage: 9-downloadfile.rb URL LOCALFILE_PATH"
+    return
+  end
+
+  url = ARGV[0]
+  local_path = ARGV[1]
+
+  puts "Downloading file from #{url}…"
+
+  URI.open(url) do |remote_file|
+    FileUtils.cp(remote_file, local_path)
+  end
+
+  puts "File downloaded and saved to #{local_path}."
+end
+
+download_file
